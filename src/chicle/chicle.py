4a1c5b20bd6cf58a77c5477a08223090850c5258
López Vinielles <gabriel.lopez@ineco.com>
2025-12-26 13:13:40 +0100

    Include first tests on chicle.chicle

--- src/chicle/chicle.py
+++ src/chicle/chicle.py
@@ -8,8 +8,9 @@ class Chicle:
     def __init__(self, data: TypeChicle) -> None:
         
         self.data = data
-        self.type = type(data)
+        self.type = type(self.data)
         self.package = self._get_package()
+        self.unprocessed_lf = self._get_df()
     
     def _get_package(self) -> str:
         return (
@@ -17,17 +18,10 @@ class Chicle:
             else 'pandas' if self.type == pd.DataFrame
             else 'python'
         )
-
-if __name__ == '__main__':
-
-    df = pl.DataFrame()
-    chicle = Chicle(df)
-    print('pl.DataFrame', chicle.package, sep='\n', end='\n')
-
-    df_dict = dict(zip(['s'], [17]))
-    chicle = Chicle(df_dict)
-    print('python dict', chicle.package, sep='\n', end='\n')
     
-    df = pd.DataFrame()
-    chicle = Chicle(df)
-    print('pd.DataFrame', chicle.package, sep='\n', end='\n')
+    def _get_df(self) -> pl.LazyFrame:
+        return (
+            self.data
+            if isinstance(self.data, pl.LazyFrame)
+            else pl.LazyFrame(self.data)
+        )
--- src/chicle/types.py
+++ src/chicle/types.py
@@ -2,4 +2,4 @@ from typing import Union, Any
 from polars import DataFrame, LazyFrame
 from pandas import DataFrame as PandasDataFrame
 
-TypeChicle = Union[dict[Any, Any], DataFrame, LazyFrame, PandasDataFrame]
+TypeChicle = Union[dict[str, Any], DataFrame, LazyFrame, PandasDataFrame]
